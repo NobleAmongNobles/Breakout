befea869da03f3db3a1ee000e825f8f78f6b2ff6
Prinz_E <prinzerik66@gmail.com>
2022-09-06 17:28:51 +0200

    added game title (final)

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -2,18 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpeedBall : Items
-{   
-     public static SpeedBall instance;
+public class Item : Items
+{    public static Item instance;
     // Start is called before the first frame update
     private void Awake(){
         instance = this;
     }
-    // Start is called before the first frame update
     void Start()
     {
         
     }
-
-   
 }
