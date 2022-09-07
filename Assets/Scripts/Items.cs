a8846969662629a0c92059a7e4c67d9c8c4cc71f
Prinz_E <prinzerik66@gmail.com>
2022-09-07 09:00:35 +0200

    removed unnecessary item script

--- Assets/Scripts/Items.cs
+++ Assets/Scripts/Items.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Items : MonoBehaviour
-{    public Items instance;
+{
+    public Items instance;
     protected int speed = 3; 
     // Start is called before the first frame update
     void Start()
     {
         
     }
+    // Update is called once per frame
     void Update()
     {
       transform.Translate(Vector2.down * Time.deltaTime * speed, 0);
@@ -17,6 +19,5 @@ public class Items : MonoBehaviour
     void Awake(){
       instance = this;
     }
-    // Update is called once per frame
    
 }
