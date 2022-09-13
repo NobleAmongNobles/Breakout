e902c6f026464d0471443f1318d476491c813458
Prinz_E <prinzerik66@gmail.com>
2022-09-13 14:44:33 +0200

    fixed game over screen and item sizes

--- Assets/Scripts/GameOverScreen.cs
+++ Assets/Scripts/GameOverScreen.cs
@@ -7,15 +7,11 @@ using System.Linq;
 
 public class GameOverScreen : MonoBehaviour
 {	
-	public Text finalscorePlayer1;
+	public Text finalScorePlayer1;
 	public Text finalScorePlayer2;
 	public Text newHighscore;
-	public Button speichern;
-	public InputField spieler;
-	public Text highScoreName;
-	public Text highscoreText1;
-	public Text highscoreText2;
-	public Text highscoreText3;
+	public Text highScoreName1;
+	public Text highScoreName2;
 
 	void Awake(){
 		if(ScoreManager.instance.score1 > ScoreManager.instance.highscores.ElementAt(9).point || ScoreManager.instance.score2 > ScoreManager.instance.highscores.ElementAt(9).point ){
@@ -23,20 +19,20 @@ public class GameOverScreen : MonoBehaviour
 		}else{
 			newHighscore.enabled = false;
 		}
-		finalscorePlayer1.text = "Final Score Player 1: " + ScoreManager.instance.score1.ToString();
+		finalScorePlayer1.text = "Final Score Player 1: " + ScoreManager.instance.score1.ToString();
 		finalScorePlayer2.text = "Final Score Player 2: " + ScoreManager.instance.score2.ToString();
 	}
 	public void NewHighscoreName1(){
-		Debug.Log(highScoreName.text);
-		ScoreManager.instance.NewHighscore(new HighscoreElement(highScoreName.text, ScoreManager.instance.score1));	
+		Debug.Log(highScoreName1.text);
+		ScoreManager.instance.NewHighscore(new HighscoreElement(highScoreName1.text, ScoreManager.instance.score1));	
 	}
 	public void NewHighscoreName2(){
-		ScoreManager.instance.NewHighscore(new HighscoreElement(highScoreName.text, ScoreManager.instance.score2));
+		ScoreManager.instance.NewHighscore(new HighscoreElement(highScoreName2.text, ScoreManager.instance.score2));
 	}
 	public void HighscoreListe(){
 		SceneManager.LoadScene(4);
 	}
-	public void Backtomenu()
+	public void BackToMenu()
 	{
 		SceneManager.LoadScene(0);
 	}
