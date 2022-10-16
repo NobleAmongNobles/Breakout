a46e05ae9908fbf9f2e7f227ca0791caa99bab96
instantcreator <dominik_czerwinski@yahoo.com>
2022-10-16 14:30:53 +0200

    A copyright free Version off the game. Do not merge!!!

--- Assets/Scripts/Backgrounds.cs
+++ Assets/Scripts/Backgrounds.cs
@@ -18,23 +18,5 @@ public class Backgrounds : MonoBehaviour
         spriteRenderer.sprite = spriteArray[background];
         
         spriteRenderer.transform.localScale = new Vector3(1.5f,1.5f,1.5f);
-        switch(background){
-            case 0:
-                spriteRenderer.color = new Color32(255,255,255,255);
-                break;
-            case 1:
-                spriteRenderer.color = new Color32(121, 0, 0, 255);
-                break;
-            case 2:
-                spriteRenderer.color = new Color32(222, 62, 166, 255);
-                break;
-            case 3:
-                spriteRenderer.color = new Color32(0, 0, 108, 255);
-                break;
-            case 4: 
-                spriteRenderer.color = new Color32(255,255,255,255);
-                break;
-
-        }
     } 
 }
